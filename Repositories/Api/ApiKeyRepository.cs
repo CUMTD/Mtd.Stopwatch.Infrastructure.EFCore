941264d247348e05f6faee501fd08761677f3ee7
Ryan Blackman <rblackman@mtd.org>
2025-08-04 14:27:01 -0500

    add developer repos

---
+++ Repositories/Api/ApiKeyRepository.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mtd.Core.Entities;
+using Mtd.Infrastructure.EFCore.Repositories;
+using Mtd.Stopwatch.Core.Entities.Api;
+using Mtd.Stopwatch.Core.Repositories.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtd.Stopwatch.Infrastructure.EFCore.Repositories.Api
+{
+	public class ApiKeyRepository(StopwatchContext context)
+		: AsyncEFRepository<ApiKey>(context), IApiKeyRepository<IReadOnlyCollection<ApiKey>>
+	{
+	{
+public Task<IReadOnlyCollection<ApiKey>> GetAllActiveWithDevelopers(CancellationToken cancellationToken) => throw new NotImplementedException();
+		public async Task<ApiKey> GetApiKey(Guid apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.SingleAsync(ak => ak.Key == GuidEntity.FormatGuid(apiKey), cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+
+		public async Task<ApiKey> GetApiKey(string apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.SingleAsync(ak => ak.Key == apiKey, cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+		public async Task<ApiKey?> GetApiKeyOrDefault(Guid apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.SingleOrDefaultAsync(ak => ak.Key == GuidEntity.FormatGuid(apiKey), cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+		public async Task<ApiKey?> GetApiKeyOrDefault(string apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.SingleOrDefaultAsync(ak => ak.Key == apiKey, cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+		public async Task<ApiKey> GetApiKeyWithDeveloper(Guid apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.Where(ak => ak.Key == GuidEntity.FormatGuid(apiKey))
+				.Include(ak => ak.Developer)
+				.SingleAsync(cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+		public async Task<ApiKey> GetApiKeyWithDeveloper(string apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.Where(ak => ak.Key == apiKey)
+				.Include(ak => ak.Developer)
+				.SingleAsync(cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+		public async Task<ApiKey?> GetApiKeyWithDeveloperOrDefault(Guid apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.Where(ak => ak.Key == GuidEntity.FormatGuid(apiKey))
+				.Include(ak => ak.Developer)
+				.SingleOrDefaultAsync(cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+		public async Task<ApiKey?> GetApiKeyWithDeveloperOrDefault(string apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.Where(ak => ak.Key == apiKey)
+				.Include(ak => ak.Developer)
+				.SingleOrDefaultAsync(cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+	}
+}
---
+++ Repositories/Api/DeveloperRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mtd.Core.Entities;
+using Mtd.Infrastructure.EFCore.Repositories;
+using Mtd.Stopwatch.Core.Entities.Api;
+using Mtd.Stopwatch.Core.Repositories.Api;
+using System.Collections.Immutable;
+
+namespace Mtd.Stopwatch.Infrastructure.EFCore.Repositories.Api
+{
+	public class DeveloperRepository(StopwatchContext context)
+		: AsyncEFIdentifiableRepository<string, Developer>(context), IDeveloperRepository<IReadOnlyCollection<Developer>>
+	{
+		public async Task<IReadOnlyCollection<Developer>> GetAllActiveWithApiKeys(CancellationToken cancellationToken)
+		{
+			var set = await _dbSet
+				.AsQueryable()
+				.Where(d => d.IsActive)
+				.Include(d => d.ApiKeys.Where(ak => ak.IsActive))
+				.ToArrayAsync(cancellationToken)
+				.ConfigureAwait(false);
+			return set
+				.ToImmutableArray();
+		}
+
+		public async Task<Developer> GetDeveloperByApiKey(string apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.Include(d => d.ApiKeys)
+				.SingleAsync(d => d.ApiKeys.Any(ak => ak.Key == apiKey), cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+
+		public async Task<Developer> GetDeveloperByApiKey(Guid apiKey, CancellationToken cancellationToken)
+		{
+			var key = GuidEntity.FormatGuid(apiKey);
+			var data = await _dbSet
+				.AsQueryable()
+				.Include(d => d.ApiKeys)
+				.SingleAsync(d => d.ApiKeys.Any(ak => ak.Key == key), cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+
+		public async Task<Developer?> GetDeveloperByApiKeyOrDefault(string apiKey, CancellationToken cancellationToken)
+		{
+			var data = await _dbSet
+				.AsQueryable()
+				.Include(d => d.ApiKeys)
+				.SingleOrDefaultAsync(d => d.ApiKeys.Any(ak => ak.Key == apiKey), cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+		public async Task<Developer?> GetDeveloperByApiKeyOrDefault(Guid apiKey, CancellationToken cancellationToken)
+		{
+			var key = GuidEntity.FormatGuid(apiKey);
+			var data = await _dbSet
+				.AsQueryable()
+				.Include(d => d.ApiKeys)
+				.SingleOrDefaultAsync(d => d.ApiKeys.Any(ak => ak.Key == key), cancellationToken)
+				.ConfigureAwait(false);
+			return data;
+		}
+	}
+}
