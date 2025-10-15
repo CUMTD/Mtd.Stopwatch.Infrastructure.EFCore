6fdb02e9fae46796c34e39c63549d805523079b0
Matthew Novelli <mattnovelli@gmail.com>
2025-10-15 10:42:09 -0500

    implement ShapePointRepository

---
+++ Repositories/Transit/ShapePointRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mtd.Infrastructure.EFCore.Repositories;
+using Mtd.Stopwatch.Core.Entities.Transit;
+using Mtd.Stopwatch.Core.Repositories.Transit;
+
+namespace Mtd.Stopwatch.Infrastructure.EFCore.Repositories.Transit
+{
+	public class ShapePointRepository(StopwatchContext context)
+		: AsyncEFRepository<ShapePoint>(context), IShapePointRepository<IReadOnlyCollection<ShapePoint>>
+	{
+
+	}
+}
